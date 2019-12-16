91a8e942cf0ed612b1b4ea0d12d39460d88073f5
Jawad <rjava721@gmail.com>
2019-12-16 11:29:03 -0600

    display movie title, genre and poster for every search

--- main.js
+++ main.js
@@ -15,6 +15,45 @@ let searchButton = document.querySelector('#searchButton');
 
 searchButton.addEventListener('click',(event) => {
   event.preventDefault();
+
   let searchText = searchBox.value;
-  console.log(searchText);
-})
+  let requestUrl = `https://www.omdbapi.com/?s=${searchText}&apikey=52df55e5`;
+  
+  // create xhr request
+  // OMDB API http://www.omdbapi.com/?i=tt3896198&apikey=52df55e5
+  let request = new XMLHttpRequest();
+
+  request.open('GET', requestUrl);
+
+  request.onload = () => {
+    let requestResponse = JSON.parse(request.responseText);
+
+    console.log(requestResponse);
+
+    requestResponse.Search.forEach((element) => {
+
+      //display search results on the page
+      if (element.Genre !== undefined) {
+        document.body.insertAdjacentHTML('beforeend', `<div>
+        <p>${element.Title}</p>
+        <p>${element.Genre}</p>
+        <img src=${element.Poster}>
+        </div>`);
+      } else {
+        document.body.insertAdjacentHTML('beforeend', `<div>
+        <p>${element.Title}</p>
+        <img src=${element.Poster}>
+        </div>`);
+      }
+
+      //add an event listener to the poster and title to show actors, plot
+      // metascore, ratings, release date
+      })
+    };
+
+    request.send();
+
+  });
+
+ 
+
